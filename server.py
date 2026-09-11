3ec479a95368c2aae1cf23c4170c96e236d87eb5
gh-ssh <sushisvk@gmail.com>
2026-09-10 21:34:57 -0400

    Task 7

--- server.py
+++ server.py
@@ -16,7 +16,10 @@ def sent_analyzer():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    if dominant_emotion == None:
+        return "Invalid text! Please try again!"
+    else:
+        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
 
 @app.route("/")
 def render_index_page():
